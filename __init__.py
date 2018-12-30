351a0608d61d8f21d229415d1fa4a20cff794a6d
astroCoder-3409 <rdeed@kc.rr.com>
2018-12-30 17:58:26 -0600

    * Cleaned up code

--- __init__.py
+++ __init__.py
@@ -34,118 +34,82 @@ class astrogpio(MycroftSkill):
 
     def __init__(self):
         super(astrogpio, self).__init__(name="astrogpio")
+
+        #initilization code for the GPIO board.
         GPIO.setmode(GPIO.BCM)
-        #GPIO.setup(signalPin, GPIO.IN)
         GPIO.setup(signalPin, GPIO.OUT)
         GPIO.setup(clockPin, GPIO.OUT)
         GPIO.output(signalPin, False)
         GPIO.output(clockPin, False)
-        self.latch = False
-        self.collector = True
 
     def initialize(self):
-        # TODO
-        #self.schedule_repeating_event(self.handle_callback, 0, 1)
         self.log.debug("Loaded!")
 
-    def handle_callback(self, message):
-        state = GPIO.input(incomingSignalPin)
-        self.log.info("Running callback:"+ str(state))
-        if not self.latch and state == 1 and not is_speaking():
-            self.speak("you got no jams")
-            if astrogpio.colorToggle:
-                self.enclosure.eyes_color(255,0,0)
-                astrogpio.colorToggle = False
-            else:
-                self.enclosure.eyes_color(0,255,0)
-                astrogpio.colorToggle = True
-
-                self.latch = True
-        else:
-            self.latch = False
-
+    #Sends GPIO command to trigger collector toggle
     @intent_file_handler('collector.intent')
     def handle_toggle_collector(self, message):
         self.speak("Received toggle")
-        self.generate_msg([0,0,1,0])
+        utt = str(message.data["utterance"])
+        self.send_msg_GPIO(4, utt)
 
+    #Sends GPIO command to trigger a ball shot
     @intent_file_handler('shooter.intent')
     def handle_shoot_ball(self, message):
         self.speak("firing ball")
-        self.generate_msg_experiment([1,1,0,0,1,0,0,0,0,0])
+        utt = str(message.data["utterance"])
+        self.send_msg_GPIO(3, utt)
 
+    #Sends GPIO command to trigger a straight drive with an distance of 0-63 inches
     @intent_file_handler('driveStraight.intent')
     def handle_drive_straight(self, message):
-        self.speak("Driving straight")
-        # self.log.info("Utterance: "+)
-        utt = message.data["utterance"]
-        dist = extract_number(str(utt))
-        binarMessage = '{0:06b}'.format(int(dist))
-        self.log.info("Binary = " + binarMessage)
-        binarMessage = binarMessage[::-1]
-        self.log.info("Dist = "+str(dist))
-        self.log.info("Reverse binary = " + binarMessage)
-        self.generate_msg_from_string('eleven', str(utt))
-        #self.generate_msg_experiment([1,1,0,0,1,0,0,0,1,0])
-    def generate_msg_from_string(self, command, msg):
-        data = exxtract_number(msg)
+        utt = str(message.data["utterance"])
+        self.send_msg_GPIO(11, utt)
+
+
+    def send_msg_GPIO(self, command, msg):
+        #extracts number from utterance
+        data = extract_number(msg)
+
+        #converts number to binary with at least 6 digits including leading zeros
         data = '{0:06b}'.format(int(data))
+
+        #reverses the order of the binary to allow the OpMode to read the information as it reads it 
         data = data[::-1]
-        command = extract_number(command)
+
+        #repeats the last two steps
         command = '{0:04b}'.format(int(command))
         command = command[::-1]
+
         self.log.info("data = " + data)
         self.log.info("command = " + command)
         self.log.info("SSS " + str(msg))
-        clockState = True
-        GPIO.output(clockPin, clockState)
-        #self.log.info("clock=" + str(bool(clockState)))
-        time.sleep(.25)
-        for i in command:
-            GPIO.output(signalPin, bool(int(i)))
-            #time.sleep(.1)
-            clockState = not clockState
-            GPIO.output(clockPin, clockState)
-            #self.log.info("clock=" + str(bool(clockState)))
-            self.log.info("SSS " + str(bool(int(i))))
-            time.sleep(.1)
-        for i in data:
-            GPIO.output(signalPin, bool(int(i)))
-            #time.sleep(.1)
-            clockState = not clockState
-            GPIO.output(clockPin, clockState)
-            #self.log.info("clock=" + str(bool(clockState)))
-            self.log.info("SSS " + str(bool(int(i))))
-            time.sleep(.1)
-        GPIO.output(signalPin, False)
-        GPIO.output(clockPin, False)
-        self.log.info("clock=" + str(bool(False)))
-        time.sleep(2.5)
-    def generate_msg(self, msg):
-        self.log.info("SSS " + str(msg))
-        GPIO.output(signalPin, True)
-        time.sleep(.25)
-        for i in msg:
-            GPIO.output(signalPin, bool(i))
-            self.log.info("SSS " + str(bool(i)))
-            time.sleep(.25)
-        GPIO.output(signalPin, False)
-        time.sleep(100)
 
-    def generate_msg_experiment(self, msg):
-        self.log.info("SSS " + str(msg))
+        #turns on the clockpin for .1s to signify a message is about to be sent
         clockState = True
         GPIO.output(clockPin, clockState)
-        self.log.info("clock=" + str(bool(clockState)))
-        time.sleep(.25)
-        for i in msg:
-            GPIO.output(signalPin, bool(i))
-            #time.sleep(.1)
-            clockState = not clockState
-            GPIO.output(clockPin, clockState)
-            self.log.info("clock=" + str(bool(clockState)))
-            self.log.info("SSS " + str(bool(i)))
+        time.sleep(.1)
+
+        for i in command:
+            # send the next bit in the 4-bit command
+            GPIO.output(signalPin, bool(int(i))) #writes the bit onto the single wire
+            clockState = not clockState #flips the clockpin tracker
+            GPIO.output(clockPin, clockState) #changes state of clockpin to match the tracker
+            self.log.info("SSS " + str(bool(int(i))))
+
+            #wait a lil bit for the OpMode to catch up
             time.sleep(.1)
+
+        for i in data:
+            # send the next bit in the 6-bit data
+            GPIO.output(signalPin, bool(int(i))) #writes the bit onto the single wire
+            clockState = not clockState #flips the clockpin tracker
+            GPIO.output(clockPin, clockState) #changes state of clockpin to match the tracker
+            self.log.info("SSS " + str(bool(int(i))))
+
+            #wait a lil bit for the OpMode to catch up
+            time.sleep(.1)
+
+        #resets both GPIO pins
         GPIO.output(signalPin, False)
         GPIO.output(clockPin, False)
         self.log.info("clock=" + str(bool(False)))
